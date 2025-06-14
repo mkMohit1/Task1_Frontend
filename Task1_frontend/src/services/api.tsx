1ed280fa30598d678fcb9dec6b28332df176fc60
Mohit Kumar <49286417+mkMohit1@users.noreply.github.com>
2025-06-15 00:05:44 +0530

    Update api.tsx

--- Task1_frontend/src/services/api.tsx
+++ Task1_frontend/src/services/api.tsx
@@ -3,7 +3,7 @@ import { User } from "../types/TableTypes";
 
 export const Users$ = new BehaviorSubject<User[]>([]);
 export const fetchUsers = async()=>{
-    const key = process.env.REACT_APP_API_KEY | '6e8902a0';
+    const key ='6e8902a0';
     const response = await fetch(`https://api.mockaroo.com/api/generate.json?key=${key}&count=100&schema=Graphkting`);
     if(!response.ok) throw new Error("Failed to fetch Data");
     const data= await response.json();
